51784f3c386011b08f662fb18096aee80de74a34
Virang Giradharbhai Virani <patelvirang9999@gmail.com>
2023-06-07 22:37:39 +0530

    on introductory problems

---
+++ basicRecursion/factorial.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    long long int factorial(int N){
+        //code here
+        if(N==0) return 1;
+        return N*factorial(N-1);
+    }
+};
